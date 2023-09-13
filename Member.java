2c73f13f813d03d6b4ac1f1c3138bcb6ad2d6988
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2023-09-13 10:47:49 +0300

    Update Member.java

--- Member.java
+++ Member.java
@@ -6,3 +6,37 @@ public class Member {
   private String name;
   private double fees;
 
+  Member(char pMemberType, int pMemberID, String pName, double pFees) {
+    this.MemberType = pMemberType;
+    this.memberID = pMemberID;
+    this.name = pName;
+    this.fees = pFees;
+  }
+
+  public void setMemberType(char pMemberType) {
+    memberType = pMemberType;
+  }
+  public char getMemberType() {
+    return memberType;
+  }
+
+  public void setMemberID(int pMemberID) {
+    memberID = pMemberID;
+  }
+  public int memberID() {
+    return memberID;
+  }
+
+  public void setName(String pName) {
+    name = pName;
+  }
+  public String getName(){
+    return name;
+  }
+  
+  public void setFees(double pFees) {
+    fees = pFees;
+  }
+  public double getFees() {
+    return fees;
+  }
