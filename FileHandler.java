6644d5f35fea429d604b83ca3e6be4e2e4cbd424
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2023-09-14 08:45:01 +0300

    Create FileHandler.java

---
+++ FileHandler.java
@@ -0,0 +1,10 @@
+package javaproject;
+
+import java.util.LinkedList;
+import java.oi.*;
+
+public class FileHandler {
+  public LinkedList<Member> readFile() {
+    LinkedList<Member> memberLinkedList = new LinkedList<>();
+  }
+}
