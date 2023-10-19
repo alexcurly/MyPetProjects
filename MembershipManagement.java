7ee2e9262b92fdefcb8e3981bd7c191480258869
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2023-10-19 08:48:31 +0300

    Update MembershipManagement.java

--- MembershipManagement.java
+++ MembershipManagement.java
@@ -8,6 +8,24 @@ final private Scanner reader = new Scanner(System.in);
 
 public class MembershipManagemen {
   private int getIntInput() {
+    int choice = 0;
+
     
+
+    while (choice == 0){
+      try{
+        //System.out.println("Input a number, please");
+        choice = reader.nextInt();
+
+        if(choice == 0) throw new InputMismatchException();
+
+        reader.nextLine(); // куда данный метод будет сохранять введенные данные ? 
+        
+        
+      }catch(InputMismatchException e){
+        System.out.println("Inputed number was wrong. Please, do it again!");
+        
+      }
+    }
   }
 }
