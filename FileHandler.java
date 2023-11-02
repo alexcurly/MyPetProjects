df17b3dc7f4fc9ea81d3390ead5232624944f8be
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2023-11-02 14:51:03 +0300

    Update FileHandler.java

--- FileHandler.java
+++ FileHandler.java
@@ -1,7 +1,8 @@
 package javaproject;
 
 import java.io.*;
-import java.util.LinkedList;
+import java.util.*;
+
 
 public class FileHandler {
 
@@ -34,45 +35,45 @@ public class FileHandler {
                             Integer.parseInt(splitLine[1]), splitLine[2],
                             Double.parseDouble(splitLine[3]),
                             Integer.parseInt(splitLine[4]));
-                    }
-                    m.add(mem);
-                    lineRead = reader.readLine();
-                  }
                 }
-      catch(IOExaption e) {
-        System.out.println(e.getMessage());
-      }
-      return m;
-    }
-
-  public void appendFile(String mem){
-    try(BufferedWriteer write = new BufferedWriter(new FileWriter("members.csv", true))) {
-      write.write(mem + "\n");
-    }
-    catch (IOExeption e) {
-      System.out.println(e.getMessage());
-    }
-  }
-
-public void overwriteFile(LinkedList<Member> m) {
-  String s;
-
-  try(BufferedWriter write = new
-      BufferedWriter(New FileWriter("member.temp", false))) {
-    for (int i=0; i< m.size(); i++)
-        {
-            s = m.get(i).toString();
-            write.write(s + "\n");
+                m.add(mem);
+                lineRead = reader.readLine();
+            }
         }
-      }catch(IOException e){
-      System.out.println(e.getMessage());
-      }
-    try{
-        File f = new File("members.csv");
-        File tf = new File("members.temp");
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return m;
+    }
 
-        f.delet();
-        tf.renameTo(f);
+    public void appendFile(String mem){
+        try(BufferedWriter write = new BufferedWriter(new FileWriter("members.csv", true))) {
+            write.write(mem + "\n");
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void overwriteFile(LinkedList<Member> m) {
+        String s;
+
+        try(BufferedWriter write = new
+                BufferedWriter(new FileWriter("member.temp", false))) {
+            for (int i=0; i< m.size(); i++)
+            {
+                s = m.get(i).toString();
+                write.write(s + "\n");
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            File f = new File("members.csv");
+            File tf = new File("members.temp");
+
+            f.delet();
+            tf.renameTo(f);
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
