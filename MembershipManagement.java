4cbe58e51500f1776c8193060cbb3e4455bce4bc
Alexandr Kyvyrzhik <69361733+alexcurly@users.noreply.github.com>
2023-11-10 10:05:24 +0300

    Update MembershipManagement.java

--- MembershipManagement.java
+++ MembershipManagement.java
@@ -16,16 +16,36 @@ public class MembershipManagemen {
       try{
         //System.out.println("Input a number, please");
         choice = reader.nextInt();
-
         if(choice == 0) throw new InputMismatchException();
-
-        reader.nextLine(); // куда данный метод будет сохранять введенные данные ? 
-        
-        
+        reader.nextLine(); // куда данный метод будет сохранять введенные данные ?         
       }catch(InputMismatchException e){
         System.out.println("Inputed number was wrong. Please, do it again!");
         
       }
     }
+    return choice;
   }
+  
+  private void printClubOptins(){
+    System.out.println("\n1) Club Mercury");
+    System.out.prntln("2) Club Neptune");
+    System.out.prntln("3) Club Jupiter");
+    System.out.prntln("4) Multi Clubs");
+  }
+
+  public int getChoice(){
+    int choice;
+    System.out.println("nWELCOME TO OZON FITNESS CENTER");
+    System.out.println("=====================");
+    System.out.println("1) Add Member");
+    System.out.println("2) Remove Member");
+    System.out.println("3) Display Member Information");
+    System.out.println("\nPlease select an option (or Enter -1 to quit): ");
+
+    choice = getIntInput();
+    return choice;    
+  }
+
+  public String add
+  
 }
